360e4a17a174a35f339d51f4505085fd2b718f9b
DEJVV3K <62350376+DEJVV3K@users.noreply.github.com>
2020-12-10 11:29:51 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+class files {
+	ifstream fileIn;
+	ofstream fileOut;
+	
+	public:
+	files(); 
+	~files();
+	void loop();
+};
+
+files::files(){
+	fileIn.open("liczby.txt");
+	fileOut.open("wynik.txt");
+}
+
+files::~files(){
+	fileIn.close();
+	fileOut.close();
+}
+
+void files::loop(){
+	while(!fileIn.eof()){
+		int liczby;
+		fileIn>>liczby;
+		if(liczby>1000){
+			fileOut<<liczby;
+			cout<<liczby<<"\n";
+		}
+	}
+}
+
+main(){
+	files liczby;
+	liczby.loop();
+	return 1;
+}
